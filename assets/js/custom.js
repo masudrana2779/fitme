440ff4ff0179da99e2128a17617ad4bb508d4429
Masud Rana <masudrana2779@gmail.com>
2020-07-04 11:34:10 +0600

    try to done design

---
+++ assets/js/custom.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+    function fitmeDegitalPlatformSlider() {
+        $(".fitmeDegitalPlatformSlider").slick({
+            centerMode: true,
+            autoplay: true,
+            centerPadding: '0px',
+            slidesToShow: 3,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '0px',
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '0px',
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        });
+    }
+    // fitmeDegitalPlatformSlider();
+
+    function DigitalContentVideoSlider(){
+        $(".DigitalContentVideoSlider").slick({
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            arrows: false,
+            dots: false,
+            focusOnSelect: true,
+            responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                }
+            }
+            ]
+        });
+    }
+    DigitalContentVideoSlider();
+});
